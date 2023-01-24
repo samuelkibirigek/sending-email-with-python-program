4820f641ae95470a9b1333b4e2962efdb3c7dacf
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-01-24 17:36:12 +0300

    Using with key word to save us the trouble of having to close connection

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ receiver_email = "receiveremail@gmail.com"
 the_message = "This email has been sent by a python program."
 
 # Google will not work without a port number.Take Note.
-connection = smtplib.SMTP("smtp.gmail.com", 587)
-# This starts the transport layer security, tls. Means if intercepted the messages will be encrypted.
-connection.starttls()
-connection.login(user=my_email, password=my_password)
-connection.sendmail(
-    from_addr=my_email,
-    to_addrs=receiver_email,
-    msg=f"Subject:Testing SMTP with Python\n\n{the_message}"
-)
-connection.close()
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    # This starts the transport layer security, tls. Means if intercepted the messages will be encrypted.
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=receiver_email,
+        msg=f"Subject:Testing SMTP with Python\n\n{the_message}"
+    )
+
 
 
